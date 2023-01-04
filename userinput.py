9112c2c5c17a6e0f8b4214c5767456517da88ba6
Nathan Wang <nathan123wang@gmail.com>
2023-01-04 22:42:08 +1000

    Grid colours

--- userinput.py
+++ userinput.py
@@ -1,14 +1,18 @@
 import pygame
-from gridgenerator import grid
+from gridgenerator import get_grid
 background_colour = (255,255,255)
 buffer = 5
 red = (255,160,122)
 
 def insert(win, position):
+    grid = get_grid()
     i,j = position[1], position[0]
     myfont = pygame.font.SysFont('Calibri', 90)
     while True:
         for event in pygame.event.get():
+            if(grid[i-1][j-1] != 0):
+                pygame.draw.rect(win, red, (position[0]*100 + 6, position[1]*100+5,100 - 2*5 , 90))
+                pygame.display.update()
             if event.type == pygame.QUIT:
                 return
             if event.type == pygame.KEYDOWN:
@@ -20,7 +24,7 @@ def insert(win, position):
                     pygame.display.update()
                     return
                 if(0 < event.key - 48 <10):  #We are checking for valid input
-                    pygame.draw.rect(win, background_colour, (position[0]*100 + 5, position[1]*100+5,100 -2*5 , 100 - 2*5))
+                    pygame.draw.rect(win, background_colour, (position[0]*100 + 6, position[1]*100+5,100 -2*5 , 100 - 2*5))
                     value = myfont.render(str(event.key-48), True, (0,0,0))
                     win.blit(value, (position[0]*100 + 30, position[1]*100 + 15))
                     grid[i-1][j-1] = event.key - 48
