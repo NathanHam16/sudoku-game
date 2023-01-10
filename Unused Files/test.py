49e2c87a379aae39ed2ff20459b774d5a90a2bb1
Nathan Wang <nathan123wang@gmail.com>
2023-01-10 13:20:37 +1000

    Clean up Files

--- Unused Files/test.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import random
-
-grid = [[(i + k) % 9 + 1 for i in range(1, 10)] for k in range(9)]
-random.shuffle(grid)
-grid = [[grid[x][y] for x in range(9)] for y in range(9)]
-print(np.matrix(grid))
